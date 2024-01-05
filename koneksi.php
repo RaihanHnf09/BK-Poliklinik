529c63c7c96083d04055e5b8bb5a4cde0d75e266
RaihanHnf09 <101207437+RaihanHnf09@users.noreply.github.com>
2024-01-06 04:51:04 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'poli';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+
+
+?>
