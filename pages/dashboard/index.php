9650d0d00440c1120a868e507b95deb394c55dec
RaihanHnf09 <101207437+RaihanHnf09@users.noreply.github.com>
2024-01-08 10:26:25 +0700

    Add files via upload

---
+++ pages/dashboard/index.php
@@ -0,0 +1,41 @@
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Dashboard</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="index.php?page=home">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+<!-- Main content -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <!-- /.card-header -->
+
+
+                    <div class="card-body"
+                        style="min-height: 400px; justify-content: center; align-items: center;">
+                        <h1 class="font-weight-bold mb-3">Hallo, <?php echo $username ?></h1><br>
+                        <h3>Selamat Datang sistem informasi Poliklinik</h3>
+                        <h4>Silahkan pilih menu pada bagian kiri</h4>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content -->
